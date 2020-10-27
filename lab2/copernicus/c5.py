8fe26e3e5c96fe74f2718bd24ff50b3b4ba048ec
krzysztof <misiek.nalepa@wp.pl>
2020-10-27 12:45:35 +0100

    bathroom done

--- lab2/copernicus/c5.py
+++ lab2/copernicus/c5.py
@@ -26,25 +26,48 @@ circuit = TkCircuit(configuration)
 @circuit.run
 def main():
     # now just write the code you would use on a real Raspberry Pi
-
     from gpiozero import LED, Button
-    from time import sleep
+    import socket
+    import struct
 
-    def button1_pressed():
-        led1.toggle()
+    MCAST_GRP = '236.0.0.0'
+    MCAST_PORT = 3456
 
-    def button2_pressed():
-        led2.toggle()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', MCAST_PORT))
+    mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    floor = 'f1'
+    room = 'bathroom'
 
     led1 = LED(21)
     led2 = LED(22)
 
     button1 = Button(11)
-    button1.when_pressed = button1_pressed
-
+    button1.when_pressed = led1.toggle
     button2 = Button(12)
-    button2.when_pressed = button2_pressed
-
+    button2.when_pressed = led2.toggle
 
     while True:
-        sleep(0.1)
+        command = sock.recv(10240).decode("utf-8").split(';')
+        print(command)
+        if command[0] == floor or command[0] == '*':
+            if command[1] == room or command[1] == '*':
+                if command[2] == 'lamp' or command[2] == '*':
+                    if command[3] == '1' or command[3] == '*':
+                        if command[4] == 'on':
+                            led1.on()
+                        elif command[4] == 'off':
+                            led1.off()
+                        elif command[4] == 'change':
+                            led1.toggle()
+                    if command[3] == '2' or command[3] == '*':
+                        if command[4] == 'on':
+                            led2.on()
+                        elif command[4] == 'off':
+                            led2.off()
+                        elif command[4] == 'change':
+                            led2.toggle()
